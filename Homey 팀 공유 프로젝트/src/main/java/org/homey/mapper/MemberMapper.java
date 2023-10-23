e2f4d24abd01efae479152a930342c1abd2eb0c8
jsh <110657773+seunghojeong01@users.noreply.github.com>
2023-10-23 18:16:18 +0900

    Add files via upload

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/MemberMapper.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/MemberMapper.java
@@ -14,9 +14,10 @@ public interface MemberMapper {
 	public int delete(String mid);//탈퇴 및 삭제
 	public int insert(MemberVO mvo);//회원 가입
 	
+	public int totalCount(ScCriteria cri);//회원수
 	//public MemberVO login(String mid, String pw);//로그인은 시큐리티가 대신 해쥼
 	public String selectId(@Param("mname") String mname,@Param("mphone") String mphone);//아이디 찾기
 	public int chkId(String mid);//아이디 중복체크 
 	public String selectPw(@Param("mid") String mid,@Param("mname") String mname,@Param("mphone") String mphone);//비번찾기에서 찾는것 비밀번호 찾아서 넘겨줄듯?
-	public int updatePw(String mid,String newPw);//비밀번호 변경
+	public int updatePw(@Param("mid") String mid,@Param("pw") String pw);//비밀번호 변경
 }
