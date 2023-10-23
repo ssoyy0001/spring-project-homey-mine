c21d0666edfc78d62f9fae4c32a60008ff1b6eb3
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-23 12:19:24 +0900

    selectItem(int consultNo) 메서드 추가

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/ConsultMapper.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/ConsultMapper.java
@@ -7,16 +7,16 @@ import org.homey.domain.Criteria;
 import org.homey.domain.ItemVO;
 
 public interface ConsultMapper {
-	
     
     public int insert(ConsultVO cvo); // 견적상담 등록
-    public int insertItem(ItemVO ivo); // ivo에 시공항목 추가
+    public int insertItem(ItemVO ivo); // 견적상담 등록과 동시에 ivo에 시공항목 추가
     
     public List<ConsultVO> selectAllPaging(Criteria cri); // 견적상담 전체 목록 페이징
     public List<ConsultVO> selectAllPagingMe(Criteria cri, String mid); // 나의 견적상담 전체 목록 페이징
     // public List<ConsultVO> selectAll(); // 견적상담 전체 목록 조회 페이징 X
 
     public ConsultVO select(int consultNo); // 견적상담 상세 조회
+    public ItemVO selectItem(int consultNo); // 시공항목 정보 조회
     //public ConsultVO select(int consultNo, String mid); // 나의 견적상담 상세 조회
     
     public int update(ConsultVO cvo); // 견적상담 수정
