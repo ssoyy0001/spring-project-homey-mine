998d4473ba5398742879b96ed41f632ddd93b553
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 02:50:04 +0900

    Update QuotationController.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/controller/QuotationController.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/controller/QuotationController.java
@@ -1,69 +1,69 @@
 package org.homey.controller;
 
-
-import java.util.List;
-
 import org.homey.domain.ItemVO;
 import org.homey.domain.QuotationVO;
+import org.homey.service.ItemService;
 import org.homey.service.QuotationService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Controller
+@RestController //JSON
 @Log4j
-@RequestMapping("/quo/*")
-@AllArgsConstructor 
+@RequestMapping("/quo/")
 public class QuotationController {
-	  private QuotationService quotationService;
-	
-	   // 견적서 등록 페이지로 이동하는 GET 요청을 처리
-	    @GetMapping("register")
-//	    @PreAuthorize("isAuthenticated") // 로그인 했을 시만 권한 적용
-	    public void register() {
-	    }
+	@Setter(onMethod_ = @Autowired)
+	private QuotationService quotationService;
+	@Setter(onMethod_ = @Autowired)
+	private ItemService itemService;
 
-	    // 견적상담을 등록하는 POST 요청을 처리
-	    @PostMapping("register")
-//	    @PreAuthorize("isAuthenticated")
-	    public void register(QuotationVO qvo, RedirectAttributes rttr) {
-	    	log.info("Quotation의 register. . .");
-	    	if (quotationService.register(qvo)) {
-	            rttr.addFlashAttribute("result", qvo.getQuoNo());
-	        } //리스트 없어서 리턴 없음
-	    }
-	    
-	    @GetMapping("view")
-	    public void view(int quoNo, Model model) {
-	    	log.info("Quotation의 view. . .");
-	        QuotationVO qvo = quotationService.view(quoNo);
-	        List<ItemVO> itemVOList = quotationService.getItemsByConsultNo(qvo.getConsultNo());
-	        model.addAttribute("qvo", qvo);
-	    }
+	// 견적서 등록 및 item 테이블 값 변경
+	@PostMapping(value = "register", produces = MediaType.TEXT_PLAIN_VALUE)
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	public ResponseEntity<String> register(@RequestBody QuotationVO qvo) { // @RequestBody는 단일객체만 받을 수 있음
+		ItemVO ivo = qvo.getItem();
+		return quotationService.register(qvo, ivo) ? new ResponseEntity<>("Success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
-	    // 견적상담을 수정하는 POST 요청을 처리
-	    @PostMapping("modify")
-//	    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-	    public void modify(QuotationVO qvo, RedirectAttributes rttr) {
-	    	log.info("Quotation의 modify. . .");
-	    	if (quotationService.modify(qvo)) {
-	            rttr.addFlashAttribute("result", "success");
-	        }
-	    }
+	// Modal 창 내에서 견적서 1개 조회 (test : http://localhost:8081/quo/38)
+	@GetMapping(value = "{quoNo}")
+	public ResponseEntity<QuotationVO> view(@PathVariable("quoNo") int quoNo) {
+		log.info("view.............");
+		return new ResponseEntity<QuotationVO>(quotationService.view(quoNo), HttpStatus.OK);
+	}
 
-	    // 견적상담을 삭제하는 POST 요청을 처리
-	    @PostMapping("remove")
-//	    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-	    public void remove(int quoNo, RedirectAttributes rttr) {
-	    	log.info("Quotation의 remove. . .");
-	    	if (quotationService.remove(quoNo)) {
-	            rttr.addFlashAttribute("result", "success");
-	        }
-	    }
-}//QuotationController end
+	// 견적서 수정
+	@PostMapping("modify")
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	@RequestMapping(method = { RequestMethod.PUT,
+			RequestMethod.PATCH }, value = "{quoNo}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> modify(@RequestBody QuotationVO qvo, @PathVariable("quoNo") int quoNo) {
+		log.info("modify.............");
+		ItemVO ivo = qvo.getItem();
+		return quotationService.modify(qvo, ivo) ? new ResponseEntity<>("Success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	// 견적서 삭제
+	@DeleteMapping(value = "{quoNo}", produces = MediaType.TEXT_PLAIN_VALUE)
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	public ResponseEntity<String> remove(@PathVariable("quoNo") int quoNo) {
+		log.info("remove.............");
+		return quotationService.remove(quoNo) ? new ResponseEntity<>("Success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}// QuotationController end
