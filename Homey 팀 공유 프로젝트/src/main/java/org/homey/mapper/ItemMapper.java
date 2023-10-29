3adca83f829fdce7b7f18ba52b77c5e7e033546a
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 03:03:25 +0900

    Update ItemMapper.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/ItemMapper.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/ItemMapper.java
@@ -9,5 +9,5 @@ public interface ItemMapper {
     public ItemVO select(int consultNo); //시공 및 철거 항목 조회
     public int update(ItemVO ivo); //시공 및 철거 항목 수정
     public int delete(int consultNo); //시공 및 철거 항목 삭제
-
+    
 }
