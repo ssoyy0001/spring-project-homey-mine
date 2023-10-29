17a1bad67dd698b79c9d2053fd72a8b259cc4748
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 03:01:48 +0900

    Update ConsultVO.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/ConsultVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/ConsultVO.java
@@ -11,8 +11,7 @@ public class ConsultVO {
 	
 	private int consultNo; // 견적상담번호(PK)
 	private String mid; //회원 id
-//	private MemberVO member; //회원 정보가 담긴 VO : 아이디, 이름을 가져올 예정
-	
+	//	private MemberVO member; //회원 정보가 담긴 VO : 아이디, 이름을 가져올 예정
     private String mphone; // 연락처
     private String postcode; // 우편 번호
     private String address; // 도로명 주소
@@ -21,24 +20,16 @@ public class ConsultVO {
     private String scheduledDate; // 공사예정일 
     private int pyeongsu; //평수
     private int budget; // 예산
-    
-    private ItemVO item;; //시공 항목들을 담은 VO
-    
+
+    private ItemVO item; //시공 항목들을 담은 VO : 1 or 0으로 insert 예정
     private String happyCall; // 해피콜 희망일시
     private Date consultDate; // 견적 상담 신청날짜
-    private String status ; // 처리현황 : 컨설트데이트와 스케줄 데이트의 값을 받아와서 바뀔것 !
+    private String status ; // 처리현황
     
-    //여기서, "하나의 '견적상담 상세조회'페이지에 
-    //견적상담 상세조회, 수정, 삭제가 가능하고, 방문실측 스케줄 등록, 수정, 삭제가 가능하고
-    //견적서 등록, 수정, 삭제가 가능하다" 라는 요구 사항
-    // 페이지가 같을 때 각 정보가 담긴vo를 list로 불러와야한다.
+    //하나의 '견적상담 상세조회'페이지에 
+    //견적상담 상세조회, 수정, 삭제 / 방문실측 스케줄 등록, 수정, 삭제 / 견적서 등록, 수정, 삭제가 가능 
+    // 페이지가 같을 때 각 정보가 담긴vo를 list로 불러옴
     private List<VisitVO> vvoList;
     private List<QuotationVO> qvoList;
-
-    //itemVO가 들어감 0원으로  0, 0, 0, 0, 0, 1 1 1 //체크를 했고
-    public void setConsultNo(int consultNo) {
-        this.consultNo = consultNo;
-    }
-
   
 }
