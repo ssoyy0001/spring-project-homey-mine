933be1356b9950dd2e0c4e86daecf3eceab9c944
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 03:07:48 +0900

    Update QuotationServiceImpl.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/QuotationServiceImpl.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/QuotationServiceImpl.java
@@ -1,12 +1,8 @@
 package org.homey.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.ibatis.session.SqlSession;
 import org.homey.domain.ItemVO;
 import org.homey.domain.QuotationVO;
+import org.homey.mapper.ItemMapper;
 import org.homey.mapper.QuotationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,30 +14,63 @@ import lombok.extern.log4j.Log4j;
 @Service					//spring에서 관리하는 요소인데 service니까 service 붙임
 @Log4j						//만약 로그기록하고싶다면 log4j
 public class QuotationServiceImpl implements QuotationService {
+	
 	@Setter(onMethod_ = @Autowired)
 	private QuotationMapper quotationMapper;
-
-	//견적서 등록 - 완
+	@Setter(onMethod_ = @Autowired)
+	private ItemMapper itemMapper;
+	
 	@Override
-	public boolean register(QuotationVO qvo) {
+	public boolean register(QuotationVO qvo, ItemVO ivo) {
 		log.info("견적서 register ServiceImpl...");
-		return quotationMapper.insert(qvo) == 1;
-	}
+		   // QuotationVO 등록
+        int result = quotationMapper.insert(qvo);
+        
+        if (result > 0) {
+        	 // QuotationVO 등록에 성공한 경우 consultNo를 얻어옴
+        	 int consultNo = qvo.getConsultNo();
+        	 
+             // 그 후 ItemVO에 consultNo 설정
+             ivo.setConsultNo(consultNo);
+             
+             // item 테이블에 데이터 업데이트
+             int itemResult = quotationMapper.updateItem(ivo);
+             if (itemResult == 1) { // 성공하면 true 반환
+                 return true;
+             }
+         }
+         return false; // 등록에 실패한 경우 false 반환
+        }
 
 	//견적서 조회 - 완
 	@Override
 	public QuotationVO view(int quoNo) {
 		log.info("견적서 view ServiceImpl...");
+	    //quotationMapper를 사용하여 QuotationVO와 관련된 정보를 조회한 후, 조회된 정보를 QuotationVO 객체에 담고, ItemVO, ConsultVO의 정보를 적절한 필드에 할당
 		return quotationMapper.select(quoNo);
 	}
 
 	//견적서 수정 - 완
 	@Override
-	public boolean modify(QuotationVO qvo) {
+	public boolean modify(QuotationVO qvo, ItemVO ivo) {
 		log.info("견적서 modify ServiceImpl...");
-		// TODO Auto-generated method stub
-		return quotationMapper.update(qvo) == 1;
-	}
+		 int result = quotationMapper.update(qvo);
+		 
+		 if (result > 0) {
+	        	 // QuotationVO 수정에 성공한 경우 consultNo를 얻어옴
+	        	 int consultNo = qvo.getConsultNo();
+	        	 
+	             // 그 후 ItemVO에 consultNo 설정
+	             ivo.setConsultNo(consultNo);
+	             
+	             // item 테이블에 데이터 업데이트
+	             int itemResult = quotationMapper.updateItem(ivo);
+	             if (itemResult == 1) { // 성공하면 true 반환
+	                 return true;
+	             }
+	         }
+	         return false; // 등록에 실패한 경우 false 반환
+	        }
 
 	//견적서 삭제 - 완
 	@Override
@@ -50,11 +79,8 @@ public class QuotationServiceImpl implements QuotationService {
 		return quotationMapper.delete(quoNo) == 1;
 	}
 
-	//일단 어떻게 쓰이는지 모르는데 - 완
-    @Override
-    public List<ItemVO> getItemsByConsultNo(int consultNo) {
-    	log.info("견적서 getItemsByConsultNo ServiceImpl...");
-    	return quotationMapper.getItemsByConsultNo(consultNo);
-    }
-
+	@Override
+	public ItemVO selectItem(int consultNo) {
+		return quotationMapper.selectItem(consultNo);
+	}
 }
