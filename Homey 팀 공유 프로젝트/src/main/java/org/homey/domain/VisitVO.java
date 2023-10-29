948d853f84781d95ddf8b15ba40bf4853653761b
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 03:02:34 +0900

    Update VisitVO.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/VisitVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/VisitVO.java
@@ -1,16 +1,17 @@
 package org.homey.domain;
 
+import java.util.Date;
+
 import lombok.Data;
 @Data
 public class VisitVO {
 	
 	// VisitVO: 방문실측 스케줄 정보
 	
+	private int visitNo; // 방문 실측 스케줄 번호 (PK)
 	private int consultNo; // 견적상담번호 (FK)
-    private int visitNo; // 방문 실측 스케줄 번호 (PK)
-    private String visitDateStart; //실측 일자 시작
-    private String visitDateEnd; //실측 일자 끝
-    private String visitStatus; //실측 여부
-    private String description; //설명
+	private String visitStatus; //실측 여부 O, X
+    private Date visitStartDate; //실측 일자
+    private Date visitEndDate; //실측 일자
   
 }
