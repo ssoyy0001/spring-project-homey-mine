f0468eb35dd7267c72bbb954bcbebc527f31054b
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 03:04:22 +0900

    Update QuotationMapper.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/QuotationMapper.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/QuotationMapper.java
@@ -1,20 +1,15 @@
 package org.homey.mapper;
 
-import java.util.List;
 
-import org.homey.domain.ConsultVO;
-import org.homey.domain.Criteria;
 import org.homey.domain.ItemVO;
 import org.homey.domain.QuotationVO;
-import org.homey.domain.VisitVO;
+
 
 public interface QuotationMapper {
-    
-    public int insert(QuotationVO qvo); // 견적서 등록
-    public QuotationVO select(int quoNo); // 견적서 조회
+	public int insert(QuotationVO qvo); // 견적서 등록
+	public int updateItem(ItemVO ivo); // 견적서 등록과 수정 시 동시에 item테이블의 값 업데이트
+    public QuotationVO select(int quoNo); // 견적서 조회 - 견적서 번호
     public int update(QuotationVO qvo); // 견적서 수정
     public int delete(int quoNo); // 견적서 삭제
-    
-    // 아이템 목록 조회
-    public List<ItemVO> getItemsByConsultNo(int consultNo);
+    public ItemVO selectItem(int consultNo); //등록된 시공항목 및 철거항목 조회
 }
