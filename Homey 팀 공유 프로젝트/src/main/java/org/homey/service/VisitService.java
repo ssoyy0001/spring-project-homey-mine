3f87ffaf1bf663c644e4ec34364fbbc623dba205
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 18:09:00 +0900

    Update VisitService.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/VisitService.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/VisitService.java
@@ -8,13 +8,13 @@ import org.homey.domain.VisitVO;
 
 public interface VisitService {
 	
-    public boolean register(VisitVO vvo);
+    public boolean register(List<VisitVO> visitList);
     public List<VisitVO> list(); // 모든 스케줄 조회 (관리자)
     public VisitVO listByConsultNo(int consultNo);// 특정 견적상담 번호를 기반으로 스케줄 조회 (회원)
 
     public VisitVO view(int visitNo); // 스케줄 상세 조회
    
-    public boolean modify(VisitVO vvo);
+    public boolean modify(List<VisitVO> visitList);
     public boolean remove(int visitNo);
     
     
