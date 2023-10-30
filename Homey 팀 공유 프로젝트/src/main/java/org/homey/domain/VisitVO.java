41f5ae5fcbcd2095031fe5e59b25ce19d8f6c048
haazzero <125067896+haazzero@users.noreply.github.com>
2023-10-30 18:07:54 +0900

    Update VisitVO.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/VisitVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/VisitVO.java
@@ -2,16 +2,29 @@ package org.homey.domain;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 @Data
 public class VisitVO {
 	
 	// VisitVO: 방문실측 스케줄 정보
 	
-	private int visitNo; // 방문 실측 스케줄 번호 (PK)
+	@JsonProperty("title") //JSON 변환 시 title로 이름을 변경하여 전달
+	private int visitNo; // 방문 실측 스케줄 번호 (PK) //Event : title
+	
 	private int consultNo; // 견적상담번호 (FK)
+	
+	@JsonProperty("description") //JSON 변환 시 description로 이름을 변경하여 전달
 	private String visitStatus; //실측 여부 O, X
-    private Date visitStartDate; //실측 일자
-    private Date visitEndDate; //실측 일자
+	
+	@JsonProperty("start")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private Date visitStartDate; //실측 일자 //Event : start
+    
+	@JsonProperty("end")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+	private Date visitEndDate; //실측 일자 //Event : end
   
 }
