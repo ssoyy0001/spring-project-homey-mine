9105c5797fa12c30a8b50949c1a0f5db4bb1c63c
ssoyy0001 <119846677+ssoyy0001@users.noreply.github.com>
2023-11-01 15:30:32 +0900

    주석 정리

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/EmailVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/EmailVO.java
@@ -7,7 +7,7 @@ public class EmailVO {
 
 	//이메일 관련 VO
 	
-	private String mailAddress;		//이메일주소
+	private String mailAddress;		//이메일주소 (From)
 	private String mailTitle;				//제목
 	private String mailContent;		//내용
 	
