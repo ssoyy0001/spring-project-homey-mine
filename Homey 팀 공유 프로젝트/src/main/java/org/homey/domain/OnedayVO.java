181434df97014c31a749508d81f7893cd1ab84c6
ssoyy0001 <119846677+ssoyy0001@users.noreply.github.com>
2023-11-01 15:33:38 +0900

    주석정리

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/OnedayVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/OnedayVO.java
@@ -23,11 +23,8 @@ public class OnedayVO {
 	private String odDeadline;						//모집마감일
 	private int odState;								//모집현황(0:모집중 / 1:모집마감)
 	private Date odWDate;							//게시글 작성일
-	private String mid;							//작성자 ID(FK)
+	private String mid;								//작성자 ID(FK)
 	private String odImg;							//이미지
 	private List<OdReqVO> odReqList;			//odNo에 대한 신청목록 -> 나의 원데이클래스 목록 조회할 때 JOIN 하려면 필요함
-	
-	//[신청하기]버튼 클릭했을 때 중복신청여부 보려고 List<odReqVO>를 가지고 있어야 하는지 고민했던 부분은
-	//Mapper에 selectAll() 메서드 추가하는 걸로 결정
 	
 }
