e118e4866a7236a25c1f236bd6966fca4db2c071
dkdjiyeon <132960473+dkdjiyeon@users.noreply.github.com>
2023-11-01 16:52:21 +0900

    sireqMapper

---
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/SiRequestMapper.java
@@ -0,0 +1,30 @@
+package org.homey.mapper;
+
+import java.util.List;
+
+import org.homey.domain.ItemVO;
+import org.homey.domain.QuotationVO;
+import org.homey.domain.SiRequestVO;
+import org.homey.domain.SireqCriteria;
+
+public interface SiRequestMapper {
+	
+	public List<SiRequestVO> getListWithPaging(SireqCriteria cri); //시공의뢰 전체목록 + 페이징
+//	public int totalCount(SireqCriteria cri); //시공의뢰 전체 수
+	
+//	public List<SiRequestVO> getList(); //시공의뢰 전체목록
+	
+	public void insert(SiRequestVO sireq); //시공의뢰 등록(insert만 처리)
+	
+	public SiRequestVO select(int quoNo);//시공의뢰 등록시 quoNo로기존값 조회
+	public int insertSelectkey(SiRequestVO sireq); //시공의뢰 등록(insert 실행 후 생성된 PK값을 알아야 하는 경우)
+	public int updateQuotationTable(QuotationVO qvo);//시공의뢰 등록시 견적서항목 업데이트
+	public int updateItem(ItemVO ivo);//시공의뢰 등록시 아이템 항목 업데이트
+	
+	public SiRequestVO read(int sireqNo); //시공의뢰 상세조회
+	public int delete(int sireqNo); //시공의뢰 삭제
+	public int update(SiRequestVO sireq); //시공의뢰 수정
+	
+	public List<SiRequestVO> myselectAllPaging(String mid, SireqCriteria cri); //나의 시공의뢰 전체목록 + 페이징
+	public int mytotalCount(String mid, SireqCriteria cri); //나의 시공의뢰 전체 수
+}
