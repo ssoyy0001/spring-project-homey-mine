99437d93ea0f403f4b73a2c5cf5e46b7b178c9e6
jsh <110657773+seunghojeong01@users.noreply.github.com>
2023-11-01 16:24:40 +0900

    Update ShowcaseController.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/controller/ShowcaseController.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/controller/ShowcaseController.java
@@ -11,7 +11,7 @@ import org.homey.domain.ScPageDTO;
 import org.homey.domain.ShowcaseAttachVO;
 import org.homey.domain.ShowcaseVO;
 import org.homey.service.ShowcaseService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.homey.service.SiScheduleService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,39 +22,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
-import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+
 @Log4j
 @Controller
 @RequestMapping("/showcase/*")
 @AllArgsConstructor
 public class ShowcaseController {
-	@Setter(onMethod_ = @Autowired)
 	private ShowcaseService showcaseService;
-	
+	private SiScheduleService sicheService;
+
 	@GetMapping("caseList")
-	public void list(Model model,ScCriteria cri) {
-		model.addAttribute("list",showcaseService.list(cri));
-		int totalCount=showcaseService.totalCount(cri);
+	public void list(Model model, ScCriteria cri) {
+		model.addAttribute("list", showcaseService.list(cri));
+		int totalCount = showcaseService.totalCount(cri);
 		log.info(cri.getBuildingType());
 		log.info(cri.getSizeRange());
 		log.info(cri.getTotalRange());
 		model.addAttribute("pageDTO", new ScPageDTO(cri, totalCount));
-		
+
 	}
-	@GetMapping({"caseView", "caseModify"})
-	public void view(int scid, Model model, 
-				     @ModelAttribute("cri") ScCriteria cri) {
-		model.addAttribute("view",showcaseService.view(scid));
-		
+
+	@GetMapping({ "caseView", "caseModify" })
+	public void view(int scid, Model model, @ModelAttribute("cri") ScCriteria cri) {
+		model.addAttribute("view", showcaseService.view(scid));
+
 	}
-	
-	
+
 	@PostMapping("caseModify")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public String modify(ShowcaseVO scvo, RedirectAttributes rttr, 
-			 @ModelAttribute("cri") ScCriteria cri) {
-		if(showcaseService.modify(scvo)) {
+	public String modify(ShowcaseVO scvo, RedirectAttributes rttr, @ModelAttribute("cri") ScCriteria cri) {
+		if (showcaseService.modify(scvo)) {
 			rttr.addFlashAttribute("msg", "수정이 완료되었습니다.");
 		}
 
@@ -64,15 +62,15 @@ public class ShowcaseController {
 		rttr.addAttribute("pageNum", cri.getPageNum());
 		rttr.addAttribute("amount", cri.getAmount());
 		return "redirect:/showcase/caseList";
-		}
+	}
+
 	@PostMapping("caseRemove")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public String remove(int scid, RedirectAttributes rttr, 
-			 @ModelAttribute("cri") ScCriteria cri) {
-		List<ShowcaseAttachVO> attachList=showcaseService.attachList(scid);
-		if(showcaseService.remove(scid)) {
+	public String remove(int scid, RedirectAttributes rttr, @ModelAttribute("cri") ScCriteria cri) {
+		List<ShowcaseAttachVO> attachList = showcaseService.attachList(scid);
+		if (showcaseService.remove(scid)) {
 			deleteFiles(attachList);
-			rttr.addFlashAttribute("msg","삭제가 완료되었습니다.");
+			rttr.addFlashAttribute("msg", "삭제가 완료되었습니다.");
 		}
 		rttr.addAttribute("buildingType", cri.getBuildingType());
 		rttr.addAttribute("sizeRange", cri.getSizeRange());
@@ -80,41 +78,40 @@ public class ShowcaseController {
 		rttr.addAttribute("pageNum", cri.getPageNum());
 		rttr.addAttribute("amount", cri.getAmount());
 		return "redirect:/showcase/caseList";
-		}
-	
-	@GetMapping("caseRegist")//여기에서 지연이 누나쪽에서  select?
+	}
+
+	@GetMapping("caseRegist") 
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public void regist() {}
-	
+	public void regist(int sicheNo, Model model) {
+		model.addAttribute("siche", sicheService.sicheView(sicheNo));// 시공팀작업의 데이터를 가져오기위함
+	}
+
 	@PostMapping("caseRegist")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	public String regist(ShowcaseVO scvo,RedirectAttributes rttr) {
-		if(showcaseService.register(scvo)) {
-			rttr.addFlashAttribute("msg","등록되었습니다.");
-		}else{
-			rttr.addFlashAttribute("msg","등록 실패!");
+	public String regist(ShowcaseVO scvo, RedirectAttributes rttr) {
+		if (showcaseService.register(scvo)) {
+			rttr.addFlashAttribute("msg", "등록되었습니다.");
+		} else {
+			rttr.addFlashAttribute("msg", "등록 실패!");
 		}
 		return "redirect:/showcase/caseList";
-		}
-public void deleteFiles(List<ShowcaseAttachVO> attachList) {
-		
-		if(attachList == null || attachList.size() == 0) {
+	}
+
+	public void deleteFiles(List<ShowcaseAttachVO> attachList) {
+
+		if (attachList == null || attachList.size() == 0) {
 			return;
 		}
-		
-		attachList.forEach(bavo->{
-			Path file = Paths.get("c:\\upload\\" + 
-									bavo.getUpFolder() + "\\" +
-									bavo.getUuid() + "_" +
-									bavo.getFileName());
+
+		attachList.forEach(bavo -> {
+			Path file = Paths
+					.get("c:\\upload\\" + bavo.getUpFolder() + "\\" + bavo.getUuid() + "_" + bavo.getFileName());
 			try {
 				Files.deleteIfExists(file);
-				
-				if(Files.probeContentType(file).startsWith("image")) {
-					Path thumbnail = Paths.get("c:\\upload\\" + 
-											bavo.getUpFolder() + "\\s_" +
-											bavo.getUuid() + "_" + 
-											bavo.getFileName());
+
+				if (Files.probeContentType(file).startsWith("image")) {
+					Path thumbnail = Paths.get(
+							"c:\\upload\\" + bavo.getUpFolder() + "\\s_" + bavo.getUuid() + "_" + bavo.getFileName());
 					Files.deleteIfExists(thumbnail);
 				}
 			} catch (IOException e) {
