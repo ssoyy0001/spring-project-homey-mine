ad875e879af679e3db527320d108c4969d9855e9
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:16:08 +0900

    최종 VisitService.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/VisitService.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/VisitService.java
@@ -2,8 +2,6 @@ package org.homey.service;
 
 import java.util.List;
 
-import org.homey.domain.ConsultVO;
-import org.homey.domain.Criteria;
 import org.homey.domain.VisitVO;
 
 public interface VisitService {
@@ -11,13 +9,8 @@ public interface VisitService {
     public boolean register(List<VisitVO> visitList);
     public List<VisitVO> list(); // 모든 스케줄 조회 (관리자)
     public VisitVO listByConsultNo(int consultNo);// 특정 견적상담 번호를 기반으로 스케줄 조회 (회원)
-
     public VisitVO view(int visitNo); // 스케줄 상세 조회
-   
     public boolean modify(List<VisitVO> visitList);
     public boolean remove(int visitNo);
     
-    
-	//서비스를 구현하는 서비스imple클래스를 만들기
-	//스프링에서 관리하는 bean으로 추가
 }
