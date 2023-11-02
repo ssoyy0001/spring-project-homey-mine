70e9820d2357155838044dd0cda3e427dbeef340
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:11:12 +0900

    최종 NoticeServiceImpl.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/NoticeServiceImpl.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/NoticeServiceImpl.java
@@ -3,67 +3,84 @@ package org.homey.service;
 import java.util.List;
 
 import org.homey.domain.Criteria;
+import org.homey.domain.NoticeAttachVO;
 import org.homey.domain.NoticeVO;
+import org.homey.mapper.NoticeAttachMapper;
 import org.homey.mapper.NoticeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Service					//spring에서 관리하는 요소인데 service니까 service 붙임
-@Log4j						//만약 로그기록하고싶다면 log4j
+@Service 
+@Log4j 
 public class NoticeServiceImpl implements NoticeService {
 	@Setter(onMethod_ = @Autowired)
 	private NoticeMapper noticeMapper;
+	@Setter(onMethod_ = @Autowired)
+	private NoticeAttachMapper naMapper;
 
+	@Transactional
 	@Override
 	public boolean register(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		boolean result = noticeMapper.insertSelectKey(nvo) == 1;
 
-	@Override
-	public List<NoticeVO> list() {
-		// TODO Auto-generated method stub
-		return null;
+		if (nvo.getAttachList() != null && nvo.getAttachList().size() > 0) {
+			nvo.getAttachList().forEach(navo -> {
+				navo.setNotNo(nvo.getNotNo());
+				naMapper.insert(navo);
+			});
+		}
+		return result;
 	}
 
 	@Override
 	public NoticeVO view(int notNo) {
-		// TODO Auto-generated method stub
-		return null;
+		NoticeVO notice = noticeMapper.select(notNo);
+		noticeMapper.noticeHit(notNo); // 조회수 증가
+		return notice;
 	}
 
 	@Override
 	public List<NoticeVO> list(Criteria cri) {
-		// TODO Auto-generated method stub
-		return null;
+		return noticeMapper.selectAllPaging(cri);
 	}
 
+	@Transactional
 	@Override
 	public boolean modify(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return false;
+
+		naMapper.deleteAll(nvo.getNotNo());
+
+		boolean result = noticeMapper.update(nvo) == 1;
+
+		noticeMapper.update(nvo);
+		if (nvo.getAttachList() != null && nvo.getAttachList().size() > 0) {
+			nvo.getAttachList().forEach(navo -> {
+				navo.setNotNo(nvo.getNotNo());
+				naMapper.insert(navo);
+			});
+		}
+		return result;
 	}
 
+	@Transactional
 	@Override
 	public boolean remove(int notNo) {
-		// TODO Auto-generated method stub
-		return false;
+		naMapper.deleteAll(notNo);
+		return noticeMapper.delete(notNo) == 1;
 	}
 
 	@Override
 	public int totalCount(Criteria cri) {
-		// TODO Auto-generated method stub
-		return 0;
+		return noticeMapper.totalCount(cri);
 	}
 
 	@Override
-	public void noticeHit(int notNo) {
-		// TODO Auto-generated method stub
-
+	public List<NoticeAttachVO> attachList(int notNo) {
+		return naMapper.select(notNo);
 	}
 
 }
