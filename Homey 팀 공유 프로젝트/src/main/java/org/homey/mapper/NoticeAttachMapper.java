7b9755c876e18d8182b701b46ba214389ce80208
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:04:30 +0900

    최종 NoticeAttachMapper.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/NoticeAttachMapper.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/mapper/NoticeAttachMapper.java
@@ -2,13 +2,13 @@ package org.homey.mapper;
 
 import java.util.List;
 
-import org.homey.domain.NoticeVO;
+import org.homey.domain.NoticeAttachVO;
 
 public interface NoticeAttachMapper {
 
-	public void insert(NoticeVO nvo);		//void
-	public List<NoticeVO> select(int notNo);
-	public void delete(String uuid);			//만약에 확인값 받고싶으면 int하지만 아님 말음 //void
-	public void deleteAll(int notNo);			//만약에 확인값 받고싶으면 int하지만 아님 말음 //void
+	public void insert(NoticeAttachVO nvo);		
+	public List<NoticeAttachVO> select(int notNo);
+	public void delete(String uuid);			
+	public void deleteAll(int notNo);
 	
 }
