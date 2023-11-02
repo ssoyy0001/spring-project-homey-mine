05e057c3a7ad93f8d24ecd1b8ab57f32c6b78d09
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:09:50 +0900

    최종 ItemServiceImpl.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/ItemServiceImpl.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/ItemServiceImpl.java
@@ -1,7 +1,6 @@
 package org.homey.service;
 
 import org.homey.domain.ItemVO;
-import org.homey.mapper.ConsultMapper;
 import org.homey.mapper.ItemMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,35 +8,33 @@ import org.springframework.stereotype.Service;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Service					//spring에서 관리하는 요소인데 service니까 service 붙임
-@Log4j						//만약 로그기록하고싶다면 log4j
+@Service					
+@Log4j						
 public class ItemServiceImpl implements ItemService {
 
 	@Setter(onMethod_ = @Autowired)
 	private ItemMapper itemMapper;
 	
 	@Override
-	public boolean register(ItemVO ivo) {
-		// TODO Auto-generated method stub
-		return false;
+	public void register(ItemVO ivo) {
+		 itemMapper.insert(ivo);
 	}
 
 	@Override
 	public ItemVO view(int consultNo) {
-		// TODO Auto-generated method stub
-		return null;
+		return itemMapper.select(consultNo);
 	}
 
 	@Override
 	public boolean modify(ItemVO ivo) {
-		// TODO Auto-generated method stub
-		return false;
+	      int updated = itemMapper.update(ivo);
+	      return updated > 0;
 	}
 
 	@Override
 	public boolean remove(int consultNo) {
-		// TODO Auto-generated method stub
-		return false;
+        int deleted = itemMapper.delete(consultNo);
+        return deleted > 0;
 	}
 
 }
