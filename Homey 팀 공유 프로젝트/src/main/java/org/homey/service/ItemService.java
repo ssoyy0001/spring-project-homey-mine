299e79268c167d67de5b3917a15043cb8718984f
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:09:19 +0900

    최종 ItemService.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/ItemService.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/ItemService.java
@@ -1,20 +1,12 @@
 package org.homey.service;
 
-import java.util.List;
-
-import org.homey.domain.Criteria;
 import org.homey.domain.ItemVO;
-import org.homey.domain.NoticeVO;
-
-
 
 public interface ItemService {
 
-    public boolean register(ItemVO ivo); 
+    public void register(ItemVO ivo);
     public ItemVO view(int consultNo); 
     public boolean modify(ItemVO ivo); 
     public boolean remove(int consultNo); 
 	
-	//서비스를 구현하는 서비스imple클래스를 만들기
-	//스프링에서 관리하는 bean으로 추가
 }
