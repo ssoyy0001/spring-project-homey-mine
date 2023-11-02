bbbd23de6676b4453a8481a5259b11583e5f3b32
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:11:56 +0900

    최종 QuotationService.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/QuotationService.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/QuotationService.java
@@ -5,10 +5,11 @@ import org.homey.domain.ItemVO;
 import org.homey.domain.QuotationVO;
 
 public interface QuotationService {
-    public boolean register(QuotationVO qvo, ItemVO ivo); // 견적서 등록 및 시공과 철거항목 업데이트
+    public boolean register(QuotationVO qvo, ItemVO ivo) throws Exception; // 견적서 등록 및 시공과 철거항목 업데이트
     public QuotationVO view(int quoNo); // 견적서 조회
-    public boolean modify(QuotationVO qvo, ItemVO ivo);// 견적서 등록 및 수정과 철거항목 업데이트
+    public boolean modify(QuotationVO qvo, ItemVO ivo) throws Exception;// 견적서 등록 및 수정과 철거항목 업데이트
     public boolean remove(int quoNo); // 견적서 삭제
     public ItemVO selectItem(int consultNo); // 시공 및 철거항목 조회
+    public boolean checkDate(QuotationVO qvo, int quoNo) throws Exception; // 시공일정 중복
 
 }
