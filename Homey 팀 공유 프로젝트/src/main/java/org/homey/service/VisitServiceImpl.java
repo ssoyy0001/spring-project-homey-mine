393ed76f227a9bd697496d4e60e9b6b4c47a26ae
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-02 10:16:56 +0900

    최종 VisitServiceImpl.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/VisitServiceImpl.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/VisitServiceImpl.java
@@ -3,7 +3,6 @@ package org.homey.service;
 import java.util.List;
 
 import org.homey.domain.VisitVO;
-import org.homey.mapper.NoticeMapper;
 import org.homey.mapper.VisitMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,75 +11,64 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@Service					//spring에서 관리하는 요소인데 service니까 service 붙임
-@Log4j						//만약 로그기록하고싶다면 log4j
+@Service
+@Log4j
 public class VisitServiceImpl implements VisitService {
 
 	@Setter(onMethod_ = @Autowired)
 	private VisitMapper visitMapper;
-	
-	//스케줄 등록
-//	@Override
-//	@Transactional 
-//	public boolean register(List<VisitVO> visitList) { //등록시 true, 아니면 false
-//		log.info("스케줄 register ServiceImpl...");
-//		return visitMapper.insert(visitList) == 1;
-//	}
+
 	@Override
-	@Transactional 
-	public boolean register(List<VisitVO> visitList) { //등록시 true, 아니면 false
+	@Transactional
+	public boolean register(List<VisitVO> visitList) { // 등록시 true, 아니면 false
 		log.info("스케줄 register ServiceImpl...");
-        for (VisitVO visit : visitList) {
-            if (visitMapper.insert(visit) != 1) {  // 만약 개별 등록에 실패하면 false 반환
-                return false;
-            }
-        }
-        return true;  // 모든 등록이 성공적으로 이루어지면 true 반환 
+		for (VisitVO visit : visitList) {
+			if (visitMapper.insert(visit) != 1) { // 만약 개별 등록에 실패하면 false 반환
+				return false;
+			}
+		}
+		return true; // 모든 등록이 성공적으로 이루어지면 true 반환
 	}
 
-	//스케줄 전체 조회 - 관리자
+	// 스케줄 전체 조회 - 관리자
 	@Override
 	public List<VisitVO> list() {
 		log.info("스케줄 관리자list ServiceImpl...");
-		 return visitMapper.selectAll();
+		return visitMapper.selectAll();
 	}
-	
+
 	// 특정 견적상담 번호를 기반으로 스케줄 조회 (회원)
 	@Override
 	public VisitVO listByConsultNo(int consultNo) {
 		log.info("스케줄 회원listByConsultNo ServiceImpl...");
-		 return visitMapper.selectByConsultNo(consultNo);
+		return visitMapper.selectByConsultNo(consultNo);
 	}
-	
-	//스케줄 상세 조회
+
+	// 스케줄 상세 조회
 	@Override
 	public VisitVO view(int visitNo) {
 		log.info("스케줄 view ServiceImpl...");
 		return visitMapper.select(visitNo);
 	}
 
-	//스케줄 수정
+	// 스케줄 수정
 	@Override
 	@Transactional
-	public boolean modify(List<VisitVO> visitList) { //수정시 true, 아니면 false
+	public boolean modify(List<VisitVO> visitList) { // 수정시 true, 아니면 false
 		log.info("스케줄 modify ServiceImpl...");
-        for (VisitVO visit : visitList) {
-            if (visitMapper.update(visit) != 1) {  // 만약 개별 등록에 실패하면 false 반환
-                return false;
-            }
-        }
-        return true;  // 모든 등록이 성공적으로 이루어지면 true 반환 
+		for (VisitVO visit : visitList) {
+			if (visitMapper.update(visit) != 1) { // 만약 개별 등록에 실패하면 false 반환
+				return false;
+			}
+		}
+		return true; // 모든 등록이 성공적으로 이루어지면 true 반환
 	}
 
-	//스케줄 삭제
+	// 스케줄 삭제
 	@Override
-	public boolean remove(int visitNo) { //삭제시 true, 아니면 false
+	public boolean remove(int visitNo) { // 삭제시 true, 아니면 false
 		log.info("스케줄 remove ServiceImpl...");
-		 return visitMapper.delete(visitNo) == 1;
+		return visitMapper.delete(visitNo) == 1;
 	}
-
-
-
-
 
 }
