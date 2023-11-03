6f699019614cefe8b45bc5ecd0438567fca4c1a5
jsh <110657773+seunghojeong01@users.noreply.github.com>
2023-11-03 09:36:37 +0900

    Update SiScheduleVO.java

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/SiScheduleVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/SiScheduleVO.java
@@ -1,33 +1,35 @@
 package org.homey.domain;
 
 import java.util.Date;
-import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
 @Data
 public class SiScheduleVO {
+	private int sicheNo;
+	private int sireqNo;
+	private int quoNo;
+	private String mid;
+	private String balanceStatus;
+	private String constructionStatus;
+	@JsonProperty("title")//json으로 값반환시 이름 지정  풀캘린더사용을 위함
+	private String workCrew;
+	@JsonProperty("start")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
+	//timezone을 Asia/Seoul로 하게되면 풀캘린더에 끝나는 날짜가 하루적게 보여지게됨 DB시간대와 안맞아서 그러는듯  
+	private Date startDate;
+	@JsonProperty("end")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
+	private Date endDate;
 	
-	private int sicheNo; // 작업등록번호 (PK)
-    private int sireqNo; // 시공의뢰번호 (FK) //혹시라도 못불러오면 vo객체로 불러오기
-    
-    private ConsultVO consultvo;// 견적상담관련 목록(FK)
-    private QuotationVO quotationvo;// 견적서관련 목록(FK)
-    
-    private Date sicheDate; // 작업등록일
-    private String balanceStatus; // 잔금처리현황
-    private String constructionStatus; // 시공진행현황
-    private String workCrew; // 작업할 시공팀
-    
-    private ItemVO ivo;// consultNo에 해당하는 아이템 목록(철거 항목/시공 항목 및 비용)
-    
-    private List<SiScheduleAttachVO> sicheAttachList;// 시공작업 첨부파일
+	private String startDateStr;//input type="date"의 값을 Date로 전환시 사용
+	private String endDateStr;
+	
+	private int total;//견적서,시공의뢰에서 값을 받기위함
+	private String buildingType;
+	private int pyeongsu;
 
-    //  private int itemNo; // 아이템 번호 (PK)   
-//    private String mid; // 회원ID (FK)
-//    private String mname; // 의뢰인이름
-//    private String constScheduleStart; // 시공일정 시작
-//    private String constScheduleEnd; // 시공일정 끝 
-//    private String buildingType; // 건물유형
-    
 }
