dd4c387fbfb9810f5de5eaf7c40563c2d2323035
ssoyy0001 <119846677+ssoyy0001@users.noreply.github.com>
2023-11-03 11:44:34 +0900

    페이징 수정

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/service/FreePdtService.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/service/FreePdtService.java
@@ -12,10 +12,9 @@ public interface FreePdtService {
 	//제품 나눔 게시글 관련 서비스단
 	
 	//페이징
-	public int fpTotalCount(SoCriteria cri);								//제품 나눔 게시글 개수
-	public List<FreePdtVO> fpListPaging(SoCriteria cri);			//제품 나눔 게시글 전체조회 , SoCriteria에는 amount, pageNum, searchOpt, searchKywd 들어있음
+	public int fpTotalCount(SoCriteria socri);								//제품 나눔 게시글 개수
+	public List<FreePdtVO> fpListPaging(SoCriteria socri);			//제품 나눔 게시글 전체조회 , SoCriteria에는 amount, pageNum, searchOpt, searchKywd 들어있음
 	
-//	public List<FreePdtVO> fpList();									//제품 나눔 게시글 전체조회 (페이징X)
 	public FreePdtVO fpView(int fpNo);								//제품 나눔 게시글 상세조회
 	public boolean fpRegister(FreePdtVO fpvo);					//제품 나눔 게시글 등록
 	public boolean fpRemove(int fpNo);								//제품 나눔 게시글 삭제
