960132224dda1531304075d89d8e5c71a04edbcb
ssoyy0001 <119846677+ssoyy0001@users.noreply.github.com>
2023-11-05 00:44:10 +0900

    set메서드 추가 -> DB에 있는 데이터 날짜가 캘린더에 그대로 뜰 수 있도록 임의로 date 설정

--- Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/SiScheduleVO.java
+++ Homey 팀 공유 프로젝트/src/main/java/org/homey/domain/SiScheduleVO.java
@@ -1,5 +1,6 @@
 package org.homey.domain;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -9,27 +10,34 @@ import lombok.Data;
 
 @Data
 public class SiScheduleVO {
-	private int sicheNo;
-	private int sireqNo;
-	private int quoNo;
-	private String mid;
-	private String balanceStatus;
-	private String constructionStatus;
-	@JsonProperty("title")//json으로 값반환시 이름 지정  풀캘린더사용을 위함
-	private String workCrew;
-	@JsonProperty("start")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-	//timezone을 Asia/Seoul로 하게되면 풀캘린더에 끝나는 날짜가 하루적게 보여지게됨 DB시간대와 안맞아서 그러는듯  
-	private Date startDate;
-	@JsonProperty("end")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
-	private Date endDate;
-	
-	private String startDateStr;//input type="date"의 값을 Date로 전환시 사용
-	private String endDateStr;
-	
-	private int total;//견적서,시공의뢰에서 값을 받기위함
-	private String buildingType;
-	private int pyeongsu;
+   private int sicheNo;
+   private int sireqNo;
+   private int quoNo;
+   private String mid;
+   private String balanceStatus;
+   private String constructionStatus;
+   @JsonProperty("title")//json으로 값반환시 이름 지정  풀캘린더사용을 위함
+   private String workCrew;
+   @JsonProperty("start")
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+   //timezone을 Asia/Seoul로 하게되면 풀캘린더에 끝나는 날짜가 하루적게 보여지게됨 DB시간대와 안맞아서 그러는듯  
+   private Date startDate;
+   @JsonProperty("end")
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+   private Date endDate;
+   
+   private String startDateStr;//input type="date"의 값을 Date로 전환시 사용
+   private String endDateStr;
+   
+   private int total;//견적서,시공의뢰에서 값을 받기위함
+   private String buildingType;
+   private int pyeongsu;
+   
+   public void setEndDate(Date endDate) {
+       Calendar cal = Calendar.getInstance();
+       cal.setTime(endDate);
+       cal.add(Calendar.DAY_OF_MONTH, 1);
+       this.endDate = cal.getTime();
+   }
 
 }
